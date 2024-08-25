253ebf118f80e87dfda6b95c47f6024b533d2c08
Jani B <aijala.j@gmail.com>
2024-08-25 15:21:00 +0300

    media query for bigger screens. link display also works now for both mobile and desktop

--- script/index.js
+++ script/index.js
@@ -1,11 +1,21 @@
 const onClickEvent = () => {
   let containerStatus = document.getElementById("person-container");
   let shareContainer = document.getElementById("share-container");
-  if (containerStatus.style.display === "none") {
-    containerStatus.style.display = "flex";
-    shareContainer.style.display = "none";
+  if (shareContainer.className === "share-container share-remove") {
+    containerStatus.classList.add("person-remove");
+    shareContainer.classList.remove("share-remove");
   } else {
-    containerStatus.style.display = "none";
-    shareContainer.style.display = "flex";
+    containerStatus.classList.remove("person-remove");
+    shareContainer.classList.add("share-remove");
+  }
+};
+
+const onClickEventWide = () => {
+  let bubble = document.getElementById("links-bubble");
+
+  if (bubble.style.display === "none") {
+    bubble.style.display = "flex";
+  } else {
+    bubble.style.display = "none";
   }
 };
